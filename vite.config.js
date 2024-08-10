c81454454bcdd8e49599e4796917f48247ab9751
Evgen <eok8177@gmail.com>
2024-08-10 16:04:55 +0300

    init

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+
+export default defineConfig({
+  plugins: [
+    laravel({
+      input: ["resources/css/admin.scss", "resources/js/admin.js"],
+      refresh: true,
+    }),
+  ],
+  build: {
+    outDir: "./public",
+    emptyOutDir: false,
+    rollupOptions: {
+        output: {
+            entryFileNames: `assets/[name].js`,
+            chunkFileNames: `assets/[name].js`,
+            assetFileNames: `assets/[name].[ext]`
+        }
+    }
+  },
+});
